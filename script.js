279e4da2447d799a3e818224507a0f5ef0cd73a4
Rain-Drops02 <57630390+Rain-Drops02@users.noreply.github.com>
2020-04-30 19:00:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(function() {
     $('.question-wrap').on('click', function(){
        $(this).next().slideToggle();
-    });
+       $(this).find('.fa-angle-down').toggleClass('fa-flip-vertical'); 
+      });
 });
